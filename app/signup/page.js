655ade1d345885e24e9e65dc830fcaf20774bde7
henglaw1991 <141654681+henglaw1991@users.noreply.github.com>
2026-04-17 19:39:05 +0700

    Add files via upload

---
+++ app/signup/page.js
@@ -0,0 +1,12 @@
+export default function SignupPage() {
+  return (
+    <iframe
+      src="/signup/index.html"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none"
+      }}
+    />
+  );
+}
